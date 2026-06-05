7c459bbc2e8d443319bb7c30519e1232142292e1
Navin-35 <163733438+ParampreetSingh24@users.noreply.github.com>
2026-06-05 18:12:32 +0530

    Implement user CRUD endpoints

--- app/main.py
+++ app/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 from app.core.config import settings
 
+from app.database.database import engine
+from app.database.base import Base
+
+import app.models
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION
